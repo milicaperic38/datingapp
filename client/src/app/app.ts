b1882cb1adf067d7e8e3e9abb899bf052d3582da
milica.peric <milica38peric15@gmail.com>
2026-03-25 14:49:14 +0100

    Nastavak

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,28 @@ export class App implements OnInit{
   protected members= signal<any>([]); // signal sluzi da kada se vrednost promeni UI se automatski osvezi gde god se taj signal koristi
   //ovaj signal je inicijalno prazan niz
   
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => this.members() = response,
-      error: error => console.log(error),
-      complete: () => console.log('Completed the http request')
-    })
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
   }
+
+  async getMembers(){
+    try{
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members')); // http vraca observable 
+    } catch (error)
+    {
+      console.log(error);
+      throw error;
+    }
+  }
+
+/*  getMembers() {
+      this.http.get('https://localhost:5001/api/members').subscribe({
+        next: (members) => {
+        console.log(members);
+      },
+      error: (error) => {
+        console.log(error);
+        }
+      });
+    }   */
 }
